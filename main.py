6572d1c69bacb1e6ac94eabb1b9b61af9b8ab8ec
iabd <iabd@TartangaLH.eus>
2023-09-27 18:41:07 +0200

    Added the first login window waiting for the database system to work

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from mainHome import *
 from classes import *
+from signUpWindow import *
 
-clase = myClass("Haizea")
-helloWorld(clase.name)
+#clase = myClass("Haizea")
+#helloWorld(clase.name)
+openSignUp()
